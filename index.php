5bf1cb6a5858125cc556daf9391423aead00fc83
DARKi <mehanik696137@gmail.com>
2025-04-03 15:50:25 +0300

    upd: структура проекта

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>КорФордж</title>
-        <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="assets/style/style.css">
     </head>
     <body>
-        
+        <button>Кнопка</button>
     </body>
 </html>
